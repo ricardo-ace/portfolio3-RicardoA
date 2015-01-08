4f73d88693bc187d7be6e253e829cedbad0a4c38
ricardo acevedo <ricardojracevedo@yahoo.com>
2015-01-08 15:02:15 -0800

    yuhg

--- public_html/script.js
+++ public_html/script.js
@@ -1,6 +1,14 @@
 
 
 $("document").ready(function(){
-    $(".yeah").css("background-color", "yellow");
-    s("div p: last-child").css("background-color","cyan");
-}); 
+    $('.yeah').css('background-color', 'blue');
+    
+    $('div:last p:nth-child(3)').css('background-color','pink');
+
+    $('div:last p:nth-child(4)').css('background-color','red');
+    
+    $('div:last p:nth-child(3)').css('border-style','solid');
+    $('div:last p:nth-child(4)').css('border-style','solid');
+    $('div:last p:nth-child(5)').css('border-style','solid');
+    
+});     
