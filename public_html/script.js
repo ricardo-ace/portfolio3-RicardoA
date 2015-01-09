794ec3c70b291372a0405dd961e763ea02a6f361
ricardo acevedo <ricardojracevedo@yahoo.com>
2015-01-09 15:01:53 -0800

    xx

--- public_html/script.js
+++ public_html/script.js
@@ -7,8 +7,27 @@ $("document").ready(function(){
 
     $('div:last p:nth-child(4)').css('background-color','red');
     
+    $('div:last p:nth-child(5)').css('background-color','white');
+    
     $('div:last p:nth-child(3)').css('border-style','solid');
     $('div:last p:nth-child(4)').css('border-style','solid');
     $('div:last p:nth-child(5)').css('border-style','solid');
     
+    
+   $('h1').bind('mouseover', mouseOverMe).bind('mouseout', mouseOutMe)
+    
+    $("h1").bind('click', mouseClick);
 });     
+
+
+function mouseOverMe(){
+    $("h1").html("this is easy");
+}
+
+function mouseOutMe(){
+    $('h1').html('do not click on these words');
+}
+
+function mouseClick(){
+    $('p').html('error, code;2323 your internet connection is not at full strength');
+}
